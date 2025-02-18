48a8d35e7481a13a2c2770e1ad5336de81c7d867
Korscao <felix.korshavn@iths.se>
2025-02-18 11:22:03 +0100

    lagt till en footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
+<template>
+	<Navbar />
+	<RouterView />
+	<Footer />
+</template>
 <script>
 import Navbar from "./components/Navbar.vue"
+import Footer from "./components/Footer.vue"
 
 export default {
 	components: {
 		Navbar,
+		Footer
 	},
 }
 </script>
-
-<template>
-	<Navbar />
-	<RouterView />
-</template>
 <style>
 	body {
 	background-color: white;
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="containerFooter">
+    <h1>Footer</h1>
+    </div>
+</template>
+<script setup>
+</script>
+<style scoped>
+    #containerFooter{
+        padding: 0;
+        margin: 0;
+        height: 100%;
+        width: 100vw;
+        background-color: black;
+        color: white;
+    }
+</style>
