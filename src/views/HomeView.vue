e98ed86558305a873a4c6fc737b739f47dfed8d1
Evelina Jonasson <idaevelina97@gmail.com>
2025-02-18 14:04:51 +0100

    Kategorier

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,41 @@
 <template>
 	<h1>Home</h1>
 	<form>
-<label for="search">Sök efter vara</label>
-<input type="text" id="searchbox" placeholder="Sök vara">
-<input type="submit" value="Sök">
-<div id="containerForAccounts">
-<button>Logga in</button>
-<button>Skapa konto</button>
-</div>
-</form>
-<label for="category">Välj kategori</label>
-<select name="category" id="category">
-<option value="Husdjur">Husdjur</option>
+		<label for="search">Sök efter vara</label>
+		<input type="text" id="searchbox" placeholder="Sök vara" />
+		<input type="submit" value="Sök" />
+		<div id="containerForAccounts">
+			<button>Logga in</button>
+			<button>Skapa konto</button>
+		</div>
+	</form>
+	<label for="category">Välj kategori</label>
+	<select v-for="category in category" name="category" id="category">
+		{{
+			item.category
+		}}
+		<!-- <option value="Husdjur">Husdjur</option>
 <option value="Fordon">Fordon</option>
 <option value="Hobby">Hobby</option>
-<option value="Kläder">Kläder</option>
-</select>
+<option value="Kläder">Kläder</option> -->
+	</select>
 
-<ItemsObject />
+	<ItemsObject />
 </template>
 
 <script setup>
-import ItemsObject from '../components/ItemsObject.vue';
-
-
+import ItemsObject from "../components/ItemsObject.vue"
 </script>
 
 <style scoped>
-#containerForAccounts{
+#containerForAccounts {
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
 	align-items: center;
 	z-index: 1;
 }
-#containerForAccounts button{
+#containerForAccounts button {
 	width: 80px;
 	height: 30px;
 	margin-top: 10px;
@@ -42,11 +43,11 @@ import ItemsObject from '../components/ItemsObject.vue';
 	font-size: 10px;
 }
 
-input[type=submit] {
-cursor: pointer;
-width: 80px;
-height: 30px;
-margin-top: 10px;
-font-size: 10px;
+input[type="submit"] {
+	cursor: pointer;
+	width: 80px;
+	height: 30px;
+	margin-top: 10px;
+	font-size: 10px;
 }
 </style>
