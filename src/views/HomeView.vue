8c53ccdd67d9446baf90ee10c05fc4eff99c1da1
Korscao <felix.korshavn@iths.se>
2025-02-18 10:52:13 +0100

    Knappar till mina sidor

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,12 @@
 	<h1>Home</h1>
 	<form>
 <label for="search">Sök efter vara</label>
-<input type="text" id="searchbox">
+<input type="text" id="searchbox" placeholder="Sök vara">
 <input type="submit" value="Submit">
+<div id="containerForAccounts">
+<button>Logga in</button>
+<button>Skapa konto</button>
+</div>
 </form>
 </template>
 
@@ -12,4 +16,17 @@
 </script>
 
 <style scoped>
+#containerForAccounts{
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+#containerForAccounts button{
+	width: 80px;
+	height: 30px;
+	margin-top: 10px;
+	cursor: pointer;
+	font-size: 10px;
+}
 </style>
