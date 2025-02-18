e7466aa7d42c3ca2356d4e9afb4b60d42d782590
Korscao <felix.korshavn@iths.se>
2025-02-18 10:21:37 +0100

    Ett test på en navbar och live share

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script>
+gu<script>
 import CitiesList from "./components/CitiesList.vue"
 import HelloWorld from "./components/HelloWorld.vue"
 import Navbar from "./components/Navbar.vue"
@@ -18,3 +18,14 @@ export default {
 	<HelloWorld msg="Hello World!" />
 	<CitiesList />
 </template>
+<style>
+	body {
+	background-color: #8186d2;
+	color: white;
+	font-family: Arial, Helvetica, sans-serif;
+	}
+
+	p {
+	color: black;
+	}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,6 @@
 <template>
+    <nav>
+    <img src="/assets/hamburgerMeny.svg" alt="" id="hamburgerIcon" @click="hamburgerMenu">
     <ul>
         <li>
             <router-link to="/">Home</router-link>
@@ -7,7 +9,35 @@
             <router-link to="/about">About</router-link>
         </li>
     </ul>
+    </nav>
 
 </template>
-<script></script>
-<style scoped></style>
+<script setup>
+import { ref } from 'vue'
+
+function hamburgerMenu() {
+    togglemenu
+}
+
+</script>
+
+<style scoped>
+
+nav {
+display: flex;
+  justify-content: space-around;
+  font-size: 20px;
+  padding: 5px;
+}
+
+#hamburgerIcon{
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+}
+
+@media screen and (min-width: 480px){
+
+}
+
+  </style>
