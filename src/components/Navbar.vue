76698d9a9fba9df22d8e84d22c6394e5e9dd4c46
Korscao <felix.korshavn@iths.se>
2025-02-18 13:04:01 +0100

    fixat i navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,18 @@
 <template>
     <nav>
     <img src="/assets/hamburgerMeny.svg" alt="ikon för hamburgarmeny" id="hamburgerIcon" @click="toggleMenu">
-    <ul v-if="hamburgerMenu" id="">
-        <li class="ListItemCategory">
+    </nav>
+    <div>
+    <ul v-if="hamburgerMenu" id="UnOrderedListCategory">
+        <li class="listItemCategory">
             <router-link to="/">Home</router-link>
         </li>
         <li class="listItemCategory">
             <router-link to="/about">About</router-link>
         </li>
     </ul>
-    </nav>
+    </div>
+
 
 </template>
 <script setup>
@@ -42,6 +45,28 @@ display: flex;
     position: absolute;
     top: 0;
     left: 0;
+}
+
+#UnOrderedListCategory{
+ z-index: 2;
+ margin: 0;
+ margin-top: 25px;
+ padding: 0;
+ width: 20vw;
+ height: 25vh;
+ background-color: rgb(255, 255, 255);
+ position: absolute;
+ top: 50;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+}
+
+.listItemCategory{
+    z-index: 2;
+    list-style-type: none;
+    margin: 10px;
+    padding: 5px;
 }
 
 @media screen and (min-width: 480px){
