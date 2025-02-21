840375bf8a4a05e30d8fd82fe3ab1f3314f4b9fb
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2025-02-21 15:25:19 +0100

    Lägg upp annons färdig med layout, funktioner måste byggas klart

--- src/views/NewProductView.vue
+++ src/views/NewProductView.vue
@@ -1,56 +1,98 @@
 <template>
-    <div id="bigWrapper">
-    <BContainer fluid>
-        <BRow>
+<div id="bigWrapper">
+<BContainer fluid>
+<h1>Lägg upp annons!</h1>
+<!--  Här startar formuläret för annonsen -->
+    <BRow>
         <BCol cols="4">
-            <h1>Lägg upp annons!</h1>
-			<BFormGroup
-				label="Namn på produkt"
-				label-for="input-1"
+        <div class="productName">
+			<BFormGroup label="Namn på produkt" label-for="input-1">
+            <BFormInput id="input-1"type="text" placeholder="Skriv här"required>
 
-                :state="state"
-			>
+            </BFormInput>
 			</BFormGroup>
-			<BFormInput
-				id="input-1"
-				type="text"
-				placeholder="Skriv här"
-				required
-                :state="state"
-			></BFormInput>
-            </BCol>
-        </BRow>
-        <BRow class="categorieSelector">
+        </div>    
+        </BCol>
+    </BRow>
+<!--  Kategoriväljare -->
+     <BRow class="productCategory">
         <BCol cols="4">
            <BFormGroup
 			label-for="input-2"
             >
-            <BFormSelect v-model="selected" :options="productCategory" />
+            <BFormSelect v-model="selectedCategory" :options="productCategory" />
             <BFormSelectOption :value="null" disabled> </BFormSelectOption>
 			</BFormGroup>
 
             </BCol>
-        </BRow>
-        <BRow >
+    </BRow>
+<!--  Skickväljare -->
+    <BRow >
         <BCol cols="4">
            <BFormGroup
 			label-for="input-3"
-            ><div class="productCondition">
-            <BFormRadio v-model="selected" name="some-radios" value="A" class="radioButton">Nyskick </BFormRadio>
-            <BFormRadio v-model="selected" name="some-radios" value="B" class="radioButton">Begangnat </BFormRadio>
-            <BFormRadio v-model="selected" name="some-radios" value="C" class="radioButton">Slitet </BFormRadio>
+            label="Skick på varan:"
+            >
+            <div class="productCondition">
+            <!--  radioknappar inuti div för att få dom i sidled -->
+            <BFormRadio v-model="selectedCondition" name="some-radios" value="A" class="radioButton">Nyskick </BFormRadio>
+            <BFormRadio v-model="selectedCondition" name="some-radios" value="B" class="radioButton">Begangnat </BFormRadio>
+            <BFormRadio v-model="selectedCondition" name="some-radios" value="C" class="radioButton">Slitet </BFormRadio>
+		    </div>
+        </BFormGroup>
+        </BCol>
+    </BRow>
+<!--  Bilduppladdning -->
+    <BRow >
+        <BCol cols="4">
+        <BFormGroup label-for="input-4" label="Ladda upp bilder på varan:">
+        <div class="productImage">
+        <input type="file" multiple @change="onFileSelected">
+        <BButton variant="outline-success" @click="onImageUpload">Ladda upp!</BButton>     
+            
 		</div>
         </BFormGroup>
+        </BCol>
+    </BRow>
+<!--  Annons Beskrivning -->
+    <BRow>
+        <BCol cols="4">
+        <div class="productDescription">
+        <BFormGroup label="Produkt beskrivning:" label-for="input-5">
+            <BFormTextarea v-model="textEx" placeholder="Nämn gärna när varan köpts, strolek på kläder osv..." rows="3" />
+        </BFormGroup>
+		</div>
+        </BCol>
+    </BRow>
+<!--  Annons Pris -->
+    <BRow>
+        <BCol cols="4">
+        <div class="productPrice">
+			<BFormGroup label="Pris på produkt" label-for="input-6">
+            <BFormInput id="input-6" type="text"v-model="formattedPrice"  placeholder="Pris här"  required>
 
-            </BCol>
-        </BRow>
+            </BFormInput>
+			</BFormGroup>
+        </div>    
+        </BCol>
+    </BRow>  
+<!--  Lägg till annons -->
+    <BRow>
+        <BCol cols="4">
+        <div class="addProduct">
+			<BFormGroup  label-for="input-7">
+            <BButton variant="success">LÄGG UPP ANNONS!🚀</BButton>
+			</BFormGroup>
+        </div>    
+        </BCol>
+    </BRow> 
 
     </BContainer>
 </div>
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import {ref, computed} from 'vue'
 
 const productCategory = [
 {value: 'null', text: 'Välj en kategori'},
@@ -61,25 +103,72 @@ const productCategory = [
 {value: 'd', text: 'Sport'},
 ]
 
-const selected = ref(null)
+const selectedCategory = ref(null)
+const selectedCondition = ref(null)
+
+let selectedFile = null;
+
+function onFileSelected(event) {
+console.log("Bild uppladdad");
+selectedFile = event.target.files[0]
+}
+
+function onImageUpload() {
+// Här ska de skrivas en cool funktion senare
+}
+
+const productPrice = ref('');
+const formattedPrice = computed({
+  get: () => (productPrice.value ? `${productPrice.value} Kr` : ''),
+  set: (value) => {
+    // ta bort bokstäver
+    productPrice.value = value.replace(/[^0-9.]/g, '');
+  }
+});
+
+
 
 </script>
 
 <style scoped>
 
-.categorieSelector {
-    padding-top: 20px;
-    padding-bottom: 20px;
+.productName {
+    margin-top: 20px;
+    margin-bottom: 30px;
+
+}
+
+.productCategory {
+    
+    
+    margin-bottom: 30px;
 }
 
 .productCondition {
     display: flex;
     margin:10px;
-    justify-content:space-between
+    justify-content:space-between;
+    margin-bottom: 30px;
 }
-.radioButton{
-    margin:100px;
-    margin-right: 10px;
+
+
+.form-check{
+    margin:0 10px 0px 10px;
+}
+
+.productImage  {
+margin-bottom:30px;
+}
+.productImage input {
+margin:10px;
+}
+
+.productDescription{
+    margin-bottom: 30px;
+}
+
+.productPrice{
+    margin-bottom: 30px;
 }
 
 #bigWrapper{
