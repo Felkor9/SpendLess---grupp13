d33e1929e69e4246133d8d49a5ffa932bba40a08
Hampus Jerkfelt <hampus.jerkfelt@gmail.com>
2025-02-26 13:20:08 +0100

    nu skickas även storlek på kläder med i formuläert

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-
+// npm-paketet som låter oss skapa en server för att få ett fungerande formulär
 import express from "express"
 
+// npm-paketet file system det låter oss skriva till itemsobjectsfilen
 import fs from "fs"
 
 import cors from "cors"
@@ -11,7 +12,7 @@ app.use(cors());
 
 const FILE_PATH = "../public/ItemsObjectData.json";
 
-// Handle form submission
+// Ta emot information från formData efter submitknappen tryckts
 app.post("/submit", (req, res) => {
   const formData = req.body;
 
@@ -26,13 +27,15 @@ app.post("/submit", (req, res) => {
     try {
       let jsonData = JSON.parse(data || '[]');
 
+      // Fälten som skrivs in i itemsfilen med info från formuläret
       const newItem = {
         id: Date.now(),
         namn: formData.productName,
         kategori: formData.selectedCategory,
         skick: formData.selectedCondition,
+        storlek: formData.selectedSize,
         beskrivning: formData.productDescription,
-        pris: Number(formData.productPrice),
+        pris: formData.productPrice,
         img: "../assets/fox.jpeg",
         adress: "activeUser.adress",
         säljare: "activeUser.name"
@@ -40,6 +43,7 @@ app.post("/submit", (req, res) => {
 
       jsonData.push(newItem);
 
+      // skriv till filen om error uppstår
       fs.writeFile(FILE_PATH, JSON.stringify(jsonData, null, 2), (writeErr) => {
         if (writeErr) {
           console.error("Write error:", writeErr);
@@ -62,5 +66,5 @@ app.use(cors({
 
 
 
-// Start server
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+// Starta servern på port 3000
+app.listen(3000, () => console.log("Servern för att formulär ska funka körs på  http://localhost:3000"));
